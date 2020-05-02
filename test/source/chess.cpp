e9af04f9a7c00322abb5660b3b78942c6d7644e1
trent.wyatt <trent@trentwyatt.com>
2020-05-02 03:30:00 -0500

    add unit tests for Move class

--- test/source/chess.cpp
+++ test/source/chess.cpp
@@ -7,6 +7,7 @@
 
 #include <board.h>
 #include <chessutil.h>
+#include <move.h>
 
 TEST_CASE("chess::chessutil") {
   using namespace chess;
@@ -68,7 +69,7 @@ TEST_CASE("chess::chessutil") {
 TEST_CASE("chess::Board") {
   using namespace chess;
 
-  // test default constrcutor
+  // test default constructor
   Board game;
 
   CHECK(game.board[0] == Rook);
@@ -88,8 +89,36 @@ TEST_CASE("chess::Board") {
   CHECK(hasMoved(game.board[0]) == false);
   CHECK(inCheck(game.board[0]) == false);
 
-  // test copy constrcutor
+  // test copy constructor
   Board game2(game);
   CHECK(isEmpty(game2.board[0]) == false);
   CHECK(isEmpty(game2.board[17]) == true);
+}
+
+TEST_CASE("chess::Move") {
+  using namespace chess;
+
+  Move move1(1, 2, 3, 4, 1000);
+
+  CHECK(move1.getFromCol() == 1);
+  CHECK(move1.getFromRow() == 2);
+  CHECK(move1.getToCol() == 3);
+  CHECK(move1.getToRow() == 4);
+
+  CHECK(move1.getFrom() == 1 + 2 * 8);
+  CHECK(move1.getTo() == 3 + 4 * 8);
+
+  CHECK(move1.getValue() == 1000);
+
+  move1.setValue(2000);
+  CHECK(move1.getValue() == 2000);
+
+  Move move2(move1);
+  CHECK(move1.getFromCol() == move2.getFromCol());
+  CHECK(move1.getFromRow() == move2.getFromRow());
+  CHECK(move1.getToCol() == move2.getToCol());
+  CHECK(move1.getToRow() == move2.getToRow());
+  CHECK(move1.getFrom() == move2.getFrom());
+  CHECK(move1.getTo() == move2.getTo());
+  CHECK(move1.getValue() == move2.getValue());
 }
